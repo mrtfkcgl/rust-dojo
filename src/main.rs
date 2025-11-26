4a7074af7fa99fd05444d8edc7b8d1d0696024f7
MrTUFEKCIOGLU <emretfkcgl@gmail.com>
2025-11-26 23:46:21 +0300

    //Rust => expect Radians not degres.     let angle_radians = angel_degress.to_radians();    angel_degress.sin()

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
-use std::io::{self, Write}; // Import the io module
+use std::io::{self, Write};
+fn main(){
+    println!("-Triangle Area Calculator (SAS Method)-");
+    println!("Formula: 0.5 * side_a * side_b * sin(angle)");
 
-fn main() {
-    println!("--- Triangle Area Calculator ---");
+    let side_a = get_input("Enter the first side lenght: ");
+    let side_b = get_input("Enter the second side lenght: ");
+    let angel_degress = get_input("Enter the angle between them (in degress): ");
 
-    // Get values by calling the helper function
-    let base = get_input("Enter the base length: ");
-    let height = get_input("Enter the height: ");
+    //Rust => expect Radians not degres.
 
-    // Calculation (using f64 allows for decimal results)
-    let area = (base * height) / 2.0;
-
+    let angle_radians = angel_degress.to_radians();
+    let area = area(side_a, side_b, angle_radians);
     println!("\nResult:");
-    println!("Triangle Area: {:.2} square units.", area);
+    println!("Side A: {}", side_a);
+    println!("Side B: {}", side_b);
+    println!("Angle : {} degrees", angle_radians);
+    println!("---------------------------------");
+    println!("Calculated Area: {:.2} square units.", area);
 }
 
-// Function to handle user input
-// This function loops until the user enters a valid number.
-fn get_input(prompt: &str) -> f64 {
-    loop {
-        print!("{}", prompt);
-        io::stdout().flush().unwrap(); // Flush stdout to display the prompt immediately
-
-        let mut input = String::new();
-
-        // Read from keyboard (stdin)
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
-
-        // Parse String to f64 number
+fn area(side_a:f64,side_b:f64,angel_degress:f64)->f64{
+    0.5*side_a*side_b*angel_degress.sin()
+}
+fn get_input(prompt: &str) -> f64{
+    loop{
+        println!("{}",prompt);
+        std::io::stdout().flush().unwrap();
+        let mut input:String = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read input)");
         match input.trim().parse::<f64>() {
-            Ok(number) => return number, // Return the number if successful
-            Err(_) => println!("Error: Please enter a valid number!"), // Warn user and restart loop
+            Ok(number) => return number,
+            Err(_) => println!("Error: Please enter a valid number!"),
         }
+
+
     }
+
 }
