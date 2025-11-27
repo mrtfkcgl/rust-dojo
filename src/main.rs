63e1aa8eb359e4d29e29793135c206888ddefb86
MrTUFEKCIOGLU <emretfkcgl@gmail.com>
2025-11-27 23:36:04 +0300

    // Calculation     // Formula: 0.5 * a * b * sin(angle)     // Important: We must convert the angle from degrees to radians!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-use std::io::{Write};
+use std::io::Write;
+
 fn main(){
-    let first_edge: f64;
-    let second_edge: f64;
-
-    print!("Plase give the first edge: ");
+    let first_side:f64;
+    let second_side:f64;
+    let angle_degree:f64;
+    print!("Please enter the first side: ");
     std::io::stdout().flush().unwrap();
-    first_edge = get_value();
-    print!("Plase give the second edge: ");
+    first_side = get_value();
+    print!("Please ente the second side: ");
     std::io::stdout().flush().unwrap();
-    second_edge = get_value();
+    second_side = get_value();
+    print!("Plaese enter the angele side:");
+    std::io::stdout().flush().unwrap();
+    angle_degree = get_value();
+    // Calculation
+    // Formula: 0.5 * a * b * sin(angle)
+    // Important: We must convert the angle from degrees to radians!
 
-    let hipo = (first_edge.powi(2)+second_edge.powi(2)).sqrt();
+    let area = 0.5*first_side*second_side*angle_degree.to_radians().sin();
 
-    println!("Hipo: {}", hipo);
+    println!("Area of the triangle: {}", area);
+
 }
 fn get_value()->f64{
-    let mut input:String=String::new();
-    std::io::stdin().read_line(&mut input).expect("Reading Err");
-    input.trim().parse().expect("Not Number")
-}
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse().expect("Not valid number")}
