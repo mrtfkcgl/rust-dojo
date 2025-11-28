759ade9d4ab97674517cdbd4b8eab34dc330d45f
MrTUFEKCIOGLU <emretfkcgl@gmail.com>
2025-11-28 23:46:34 +0300

    Exercise: Sum of Digits

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use std::io::Write;
 
 fn main(){
+    let mut value:i32;
 
-    let input_Number:i32;
-
-    print!("Lütfen bir sayı giriniz:");
+    print!("Number: ");
     std::io::stdout().flush().unwrap();
-    input_Number = get_val();
+    let mut digit:i32;
+    let mut sum:i32 = 0i32;
+    value = get_value();
 
+    while value > 0 {
 
-    for i in 1..=input_Number{
-        if input_Number % i == 0{
-            println!("{}",i);
-        }
+        digit = value % 10;
+        sum += digit;
+        value /= 10;
+
     }
+    println!("Sum of elements: {}",sum);
 
 
 }
 
-fn get_val()->i32{
+fn get_value()->i32{
+    let value:i32;
     let mut s:String = String::new();
-    std::io::stdin().read_line(&mut s).expect("Reading err");
-    s.trim().parse().expect("Not a number")
+    std::io::stdin().read_line(&mut s).expect("Readign err");
+    value = s.trim().parse().expect("Parsing err");
+    value
 }
